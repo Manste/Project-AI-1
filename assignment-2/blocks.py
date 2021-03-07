f915cf83e62395d0e707f07f790495b5a16687c0
Xavier Claude <xavierclaude0@gmail.com>
2021-03-07 23:27:07 +0100

    Update blocks.py

--- assignment-2/blocks.py
+++ assignment-2/blocks.py
@@ -3,6 +3,7 @@
 from search import *
 import sys
 import time
+import copy
 
 goal_state = None
 #################
@@ -11,10 +12,50 @@ goal_state = None
 class Blocks(Problem):
 
     def successor(self, state):
-        pass
+        for pos in state.blocks_positions:
+            for move in ((0,1),(0,-1)):
+
+
+                newR = pos[0] + move[0]
+                newC = pos[1] + move[1]
+
+                possible_move = newR>=0 and newR<state.nbr and newC>=0 and newC<state.nbc and state.grid[newR][newC]==' '
+
+
+                if(possible_move):
+
+                    new_state = State(copy.deepcopy(state.grid))
+
+                    # Falling
+                    while(newR<state.nbr-1 and state.grid[newR+1][newC]==' '):
+                            newR = newR+1
+
+                    #Matching
+                    if(state.grid[pos[0]][pos[1]].upper()==goal_state.grid[newR][newC]):
+                        new_state.grid[newR][newC] = '@'
+                    #Moving    
+                    else:
+                        new_state.grid[newR][newC] = state.grid[pos[0]][pos[1]]
+                        new_state.blocks_positions.append((newR,newC))
+
+                    #Erasing last position in any case
+                    new_state.grid[pos[0]][pos[1]] = ' '  
+                    new_state.blocks_positions.remove(pos)          
+
+                    yield (move,new_state)
+
+        
 
     def goal_test(self, state):
-        pass
+        for i in range(state.nbr):
+            for j in range(state.nbc):
+                if state.grid[i][j] not in ['#','@',' ']:
+                    #print("Failed",state.grid[i][j])
+                    #print(state)
+                    return False
+        print("Solution found")
+        return True
+
 
 
 ###############
@@ -25,6 +66,11 @@ class State:
         self.nbr = len(grid)
         self.nbc = len(grid[0])
         self.grid = grid
+        self.blocks_positions = []
+        for i in range(self.nbr):
+            for j in range(self.nbc):
+                if grid[i][j] not in ['#',' ']:
+                    self.blocks_positions.append((i,j))
 
     def __str__(self):
         n_sharp = self.nbc + 2
@@ -37,6 +83,12 @@ class State:
             if i < self.nbr - 1:
                 s += '\n'
         return s + "\n" + "#" * n_sharp
+
+    def __eq__(self, other):
+        return self.grid == other.grid
+
+    def __hash__(self):
+        return hash(tuple(tuple(lst) for lst in self.grid))
 
 
 ######################
@@ -61,18 +113,25 @@ def heuristic(node):
 ##############################
 #Use this block to test your code in local
 # Comment it and uncomment the next one if you want to submit your code on INGInious
-'''instances_path = "instances/"
-instance_names = ['a01','a02','a03','a04','a05','a06','a07','a08','a09','a10','a11']
+instances_path = "instances/"
+instance_names = ['a02']
+#instance_names = ['a01','a02','a03','a04','a05','a06','a07','a08','a09','a10','a11']
 
 for instance in [instances_path + name for name in instance_names]:
     grid_init, grid_goal = readInstanceFile(instance)
     init_state = State(grid_init)
     goal_state = State(grid_goal)
+
+    #print(init_state.blocks_positions)
     problem = Blocks(init_state)
+    '''
+    for s in problem.successor(init_state):
+        print(s[1])
+    '''
 
     # example of bfs tree search
     startTime = time.perf_counter()
-    node, nb_explored, remaining_nodes = depth_first_graph_search(problem)
+    node, nb_explored, remaining_nodes = breadth_first_graph_search(problem)
     endTime = time.perf_counter()
 
     # example of print
@@ -86,13 +145,14 @@ for instance in [instances_path + name for name in instance_names]:
     print("* Execution time:\t", str(endTime - startTime))
     print("* Path cost to goal:\t", node.depth, "moves")
     print("* #Nodes explored:\t", nb_explored)
-    print("* Queue size at goal:\t",  remaining_nodes)'''
+    print("* Queue size at goal:\t",  remaining_nodes)
 
 
 
 ####################################
 # Launch the search for INGInious  #
 ####################################
+'''
 #Use this block to test your code on INGInious
 instance = sys.argv[1]
 grid_init, grid_goal = readInstanceFile(instance)
@@ -117,3 +177,4 @@ print("* Execution time:\t", str(endTime - startTime))
 print("* Path cost to goal:\t", node.depth, "moves")
 print("* #Nodes explored:\t", nb_explored)
 print("* Queue size at goal:\t",  remaining_nodes)
+'''
